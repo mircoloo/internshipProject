78edccd4b4a15e039cf7b3249bf4282d052a1ee3
mircoloo <bismirco@gmail.com>
2022-03-06 20:50:47 +0100

    prova conversione to json

--- provaTwitter.py
+++ provaTwitter.py
@@ -9,10 +9,12 @@ query = "SMS truffa"
 
 response = client.search_recent_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang', 'context_annotations', 'entities', 'public_metrics'])
 
-columns = ['Time', 'Tweet', 'Lang', 'User']
+columns = ['Time', 'Tweet', 'Lang']
 data = []
 for tweet in response.data:
-    data.append([tweet.created_at, tweet.text, tweet.lang,tweet.entities['mentions']  if 'mentions' in tweet.entities else "--"])
+    data.append([tweet.created_at, tweet.text, tweet.lang])#tweet.entities['mentions']  if 'mentions' in tweet.entities else "--"])
 
 df = pd.DataFrame(data,columns=columns)
 print(df)
+
+df.to_json('twi.json', orient='records', lines=True)
