923253e67e4bfe2004283585d4e8cd7f34595406
mircoloo <bismirco@gmail.com>
2022-03-07 11:19:51 +0100

    Update twitter

--- pyScripts/twitter.py
+++ pyScripts/twitter.py
@@ -2,21 +2,28 @@
 import pandas as pd
 import tweepy
 import ApiKeys.apiKey as k
+import json
+import unidecode
 
 client = tweepy.Client(bearer_token=k.BEARER_TOKEN)
 
-query = "SMS truffa"
+def getTweets(maxResults=10):
 
-response = client.search_recent_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang', 'context_annotations', 'entities', 'public_metrics'])
+    query = "SMS truffa -isRetweet"
+    response = client.search_recent_tweets(query=query, max_results=maxResults, tweet_fields=['created_at', 'lang', 'context_annotations', 'entities', 'public_metrics'])
+    return response
+def dFtoJSON(file_name: str, df):
+    df.to_json(file_name, orient='records', lines=True, force_ascii=True)
+
 
 columns = ['Time', 'Tweet', 'Lang']
 data = []
+response = getTweets()
 for tweet in response.data:
     data.append([str(tweet.created_at)[:10], str(tweet.text), tweet.lang])#tweet.entities['mentions']  if 'mentions' in tweet.entities else "--"])
-    print(tweet.text)
+    #print(tweet.text)
 df = pd.DataFrame(data,columns=columns)
+
+dFtoJSON('tweets.json', df)
+
 print(df)
-
-
-
-df.to_json('tweets.json', orient='records', lines=True)
