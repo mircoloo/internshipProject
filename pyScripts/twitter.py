f7293767feaf733a56601f07b2d12cc271ac2506
mircoloo <bismirco@gmail.com>
2022-03-07 10:58:15 +0100

    twitter.py update

--- pyScripts/twitter.py
+++ pyScripts/twitter.py
@@ -1,41 +1,22 @@
 #!python3
-import ApiKeys.apiKey as k
-import tweepy
 import pandas as pd
+import tweepy
+import ApiKeys.apiKey as k
 
-def getClient():
-    client = tweepy.Client(bearer_token=k.BEARER_TOKEN, consumer_key=k.API_KEY, consumer_secret=k.API_SECRET_KEY, access_token=k.ACCESS_TOKEN, access_token_secret=k.ACCESS_SECRET_TOKEN)
-    return client
+client = tweepy.Client(bearer_token=k.BEARER_TOKEN)
 
-def searchTweets(query, maxResults=10):
-    client = getClient()
-    tweets = client.search_recent_tweets(query=query, max_results=maxResults)
-    #tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations','created_at'],media_fields=['preview_image_url'], max_results=maxResults)
-    tweet_data = tweets.data
-    result = []
-    if not tweet_data is None and len(tweet_data) > 0:
-        for tweet in tweet_data:
-            obj = {}
-            obj['ID'] = tweet.id
-            obj['text'] = tweet.text
-            result.append(obj)
-    else:
-        return ''
-    return result
+query = "SMS truffa"
 
-for x in searchTweets("truffa sms"):
-    print(x)
+response = client.search_recent_tweets(query=query, max_results=10, tweet_fields=['created_at', 'lang', 'context_annotations', 'entities', 'public_metrics'])
 
-#prove conversione to JSON 
-""" json_file = df.to_json(orient='records')
-fileHandler = open("tweet_json.json", "a+")
-fileHandler.write(json_file) """
-
-    
+columns = ['Time', 'Tweet', 'Lang']
+data = []
+for tweet in response.data:
+    data.append([str(tweet.created_at)[:10], str(tweet.text), tweet.lang])#tweet.entities['mentions']  if 'mentions' in tweet.entities else "--"])
+    print(tweet.text)
+df = pd.DataFrame(data,columns=columns)
+print(df)
 
 
 
-
-
-
-
+df.to_json('tweets.json', orient='records', lines=True)
