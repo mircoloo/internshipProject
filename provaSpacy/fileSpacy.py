9b1227d96b73328e891be1e0bcabbcb99c5c1286
mircoloo <bismirco@gmail.com>
2022-03-09 10:22:25 +0100

    ssd

--- provaSpacy/fileSpacy.py
+++ provaSpacy/fileSpacy.py
@@ -1,11 +1,9 @@
 #!python3
-
-from spacy import displacy
 import spacy
 
 
 
-nlp = spacy.load("it_core_news_sm")
+nlp = spacy.load('it_core_news_sm')
 
 #text = "A Mirco piace partecipare a questo progetto delegato da FBK in quanto non sta capendo nulla di quello che sta facendo. Gli piace programmare in diversi linguaggi, studia ingegneria informatica e spera di laurearsi a breve"
 text = open('tweets.txt', 'r').read()
@@ -14,8 +12,8 @@ doc = nlp(text)
 
 
 
-for sent in doc.sents:
-    print(sent)
+for token in doc:
+    print(token.ent_type_)
     
 
 
