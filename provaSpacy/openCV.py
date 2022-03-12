c2960fce4e580a5a8f953e2086f8d182e6fe8d51
mircoloo <bismirco@gmail.com>
2022-03-12 10:28:01 +0100

    asd

--- provaSpacy/openCV.py
+++ provaSpacy/openCV.py
@@ -1,10 +1,10 @@
 #!python3
 import pytesseract
 import cv2
-import time
 import spacy
 
-img = cv2.imread('image.jpg')
+img = cv2.imread('provaSpacy/image.jpg')
+
 
 nlp = spacy.load('it_core_news_sm')
 
@@ -12,11 +12,12 @@ nlp = spacy.load('it_core_news_sm')
 # Adding custom options
 custom_config = r'--oem 3 --psm 6'
 text = pytesseract.image_to_string(img, config=custom_config)
-print(img)
-print(text)
 
 t = nlp(text)
 
-cv2.imshow('Immagine 1', img)
+for token in t:
+    print(token, token.morph)
 
-cv2.waitKey(0)
+""" cv2.imshow('Immagine 1', img)
+
+cv2.waitKey(0) """
