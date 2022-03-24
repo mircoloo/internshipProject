1da43a7ad2fd4fbd14d3bc66c44941fd3c895afa
mircoloo <bismirco@gmail.com>
2022-03-24 17:13:00 +0100

    added image url

--- proveTwitt.py
+++ proveTwitt.py
@@ -1,25 +1,27 @@
 #!python3
-
-from xml.etree.ElementInclude import include
 import tweepy
 import Prove.pyScripts.ApiKeys.apiKey as k
 
 #https://twitter.com/consumatorirt/status/
 
-query = 'covid lang:it has:images'    #from:____   #has:_____
+query = 'SMS truffa'    #from:____   #has:_____
 client = tweepy.Client(bearer_token=k.BEARER_TOKEN, consumer_key=k.API_KEY, consumer_secret=k.API_SECRET_KEY, access_token=k.ACCESS_TOKEN, access_token_secret=k.ACCESS_SECRET_TOKEN)
-response = client.search_recent_tweets(query=query, tweet_fields=['geo', 'created_at', 'attachments', 'id', 'author_id', 'lang', 'public_metrics','source'],  media_fields=['preview_image_url', 'media_key', 'url'],expansions=['attachments.media_keys', 'geo.place_id', 'author_id'], max_results=10, user_fields=['profile_image_url'])
-#response = client.get_tweet(id='1506556446279970816', tweet_fields=['geo', 'created_at', 'attachments', 'id', 'author_id', 'lang', 'public_metrics','source'],  media_fields=['preview_image_url', 'media_key', 'url'],expansions=['attachments.media_keys'],user_fields=['profile_image_url'])
+response = client.search_recent_tweets(max_results=10, query=query, tweet_fields= ['created_at', 'attachments', 'id', 'public_metrics'],  media_fields=['url'],expansions=['attachments.media_keys'])
+#response = client.get_tweet(id='1506556446279970816', tweet_fields=['entities','geo', 'created_at', 'attachments', 'id', 'author_id', 'lang', 'public_metrics','source', 'possibly_sensitive'],  media_fields=['preview_image_url', 'media_key', 'url'],expansions=['attachments.media_keys', 'geo.place_id', 'author_id'], user_fields=['profile_image_url'])
 
  
 #imgs = {u['url']: u for u in response.includes['media']}
 
-for i, img in enumerate(response.includes['media']):
-    #print(i, img.url)
-    #print(response.includes['media'])
-    pass
-for tweet in response:
-    print(tweet)
 
-#print(imgs)
+
+for i, tweet in enumerate(response.data):
+    print(i, tweet.id)
+    if(tweet.attachments):
+        media_key = tweet.attachments['media_keys']
+        for el in response.includes['media']:
+            if media_key[0] == el.media_key and el.type == 'photo':
+                print(i, el.url)
+
+
+
 
--- twitt.py
+++ twitt.py
@@ -1,11 +1,13 @@
 #!python3
 import pandas as pd
 import Prove.pyScripts.ApiKeys.apiKey as k
-import pandas as pd
 import tweepy
 
 
-
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option('display.max_colwidth', -1)
 
 """    ######PROVE RETREIVING IMMAGINI######
 
@@ -54,20 +56,27 @@ client = tweepy.Client(bearer_token=k.BEARER_TOKEN)
 query = 'SMS truffa '
 
 #results of the query
-results = client.search_recent_tweets(query=query,  tweet_fields=['geo', 'created_at', 'attachments', 'id'], 
-                            media_fields=['preview_image_url'],expansions=['attachments.media_keys'], max_results=15)
+results = client.search_recent_tweets(query=query,  tweet_fields=['geo', 'created_at', 'attachments', 'id', 'entities'], 
+                            media_fields=['preview_image_url', 'url'],expansions=['attachments.media_keys'], max_results=15)
 
 #iterate from all the tweet and retrieve information | twitter comment | tweet id | tweet image text | ... | time | image url 
 data = []
-for tweet in results.data:
-    #print(tweet.text, '\n')
-    #print(tweet['geo'])
-    #print(tweet.data['context_annotations'])
-    print(tweet.text)
-    #data.append([tweet.id, tweet['text'], tweet.data['created_at'][:-14]])
+#imgs = {u['url']: u for u in results.includes['media']}
 
 
-#df = pd.DataFrame(data, columns=['ID', 'Comment', 'Creation'])
 
-#print(df)
+for i,tweet in enumerate(results.data):
+    inserted = False
+    if(tweet.attachments):
+        media_key = tweet.attachments['media_keys']
+        for el in results.includes['media']:
+            if media_key[0] == el.media_key and el.type == 'photo':
+                data.append([tweet.id, tweet['text'], tweet.data['created_at'][:-14], el.url])
+                inserted=True
+    if not inserted:
+        data.append([tweet.id, tweet['text'], tweet.data['created_at'][:-14], 'NI'])
+
+#1506665344395452418
+df = pd.DataFrame(data, columns=['ID', 'Comment', 'Creation', 'ImageUrl'])
+print(df)
 
