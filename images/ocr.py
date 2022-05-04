278cf96c2d5d490252e5388654061158949a8704
mircoloo <bismirco@gmail.com>
2022-05-04 13:57:09 +0200

    upd da ub

--- images/ocr.py
+++ images/ocr.py
@@ -1,4 +1,5 @@
 #!python3
+from time import sleep
 import pytesseract
 import cv2
 import os
@@ -6,15 +7,19 @@ import os
 
 #pytesseract.pytesseract.tesseract_cmd = r"/usr/local/bin/tesseract"
 
-img_path = "sample_image2.png"
+full_img_path = "/opt/lampp/htdocs/internshipProject/images/sample_image0.png"
 
-img = cv2.imread(img_path)
 
-cv2.imshow("image", img)
+NUMBER_OF_IMAGES = 5
 
-""" config = ('-l eng --oem 1 --psm 3')
-text = pytesseract.image_to_string(img, config=config)
-print(img) """
+for i in range(NUMBER_OF_IMAGES):
+    img_path = f"images/sample_image{i}.png"
+    img = cv2.imread(img_path)
+    config = ('-l eng --oem 1 --psm 3')
+    text = pytesseract.image_to_string(img, config=config)
+    file = open(f"images/sample_text{i}.txt", "w+")
+    file.write(text)
+    file.close()
 
 
 
